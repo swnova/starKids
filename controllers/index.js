e89c982f3e1076d6d1da4008c1f48ba109bb60c1
michelle <113373744+mfarrell23@users.noreply.github.com>
2022-11-09 14:47:58 -0700

    adding starRoute basic

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const kidRoutes = require('./kidsRoutes');
 const taskRoutes= require('./taskRoutes');
-const frontEndRoutes = require("./frontEndRoutes")
+const frontEndRoutes = require("./frontEndRoutes");
+const starRoutes= require('./starRoutes')
 
 router.use('/users', userRoutes);
 router.use('/kids', kidRoutes);
